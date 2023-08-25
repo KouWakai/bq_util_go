bc7078736ff564e13ff39b4f58d9234b0246d05c
kouwakai <kouyama112@gmail.com>
2023-08-25 12:52:34 +0900

    load_schemaをクリーンアーキテクチャでリファクタした

--- change_table_schema/load_schema.go
+++ change_table_schema/load_schema.go
@@ -10,35 +10,108 @@ import (
 	"cloud.google.com/go/bigquery"
 )
 
-func Load(project_id string, load_datasetid string, load_tableid string, schema_path string) {
-	// Google Cloudのクライアントを作成します
-	ctx := context.Background()
-	client, err := bigquery.NewClient(ctx, project_id)
+type FieldTypeMapper interface {
+	FieldTypeToString(fieldType bigquery.FieldType) string
+}
+
+type BigQueryClient interface {
+	GetTableMetadata(ctx context.Context, projectID, datasetID, tableID string) (*bigquery.TableMetadata, error)
+}
+
+type FieldInfo struct {
+	Name string `json:"Name"`
+	Type string `json:"Type"`
+}
+
+type SchemaUseCase struct {
+	bqClient     BigQueryClient
+	fieldTypeMap FieldTypeMapper
+}
+
+func NewSchemaUseCase(bqClient BigQueryClient, fieldTypeMap FieldTypeMapper) *SchemaUseCase {
+	return &SchemaUseCase{
+		bqClient:     bqClient,
+		fieldTypeMap: fieldTypeMap,
+	}
+}
+
+func (uc *SchemaUseCase) GenerateAndSaveSchema(ctx context.Context, projectID, datasetID, tableID, schemaPath string) error {
+	meta, err := uc.bqClient.GetTableMetadata(ctx, projectID, datasetID, tableID)
 	if err != nil {
-		log.Fatalf("Failed to create BigQuery client: %v", err)
+		return fmt.Errorf("failed to get table metadata: %v", err)
 	}
 
-	// スキーマを取得するテーブルの情報を指定します
+	var fields []FieldInfo
+	for _, field := range meta.Schema {
+		fields = append(fields, FieldInfo{
+			Name: field.Name,
+			Type: uc.fieldTypeMap.FieldTypeToString(field.Type),
+		})
+	}
+
+	fieldsJSON, err := json.MarshalIndent(fields, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal fields to JSON: %v", err)
+	}
+
+	err = ioutil.WriteFile(schemaPath, fieldsJSON, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write fields to file: %v", err)
+	}
+
+	return nil
+}
+
+type FieldTypeMap struct{}
+
+func NewFieldTypeMap() *FieldTypeMap {
+	return &FieldTypeMap{}
+}
+
+func (fm *FieldTypeMap) FieldTypeToString(fieldType bigquery.FieldType) string {
+	switch fieldType {
+	case bigquery.StringFieldType:
+		return "STRING"
+	case bigquery.IntegerFieldType:
+		return "INTEGER"
+	case bigquery.FloatFieldType:
+		return "FLOAT"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+type BigQueryClientImpl struct{}
+
+func NewBigQueryClientImpl() *BigQueryClientImpl {
+	return &BigQueryClientImpl{}
+}
+
+func (c *BigQueryClientImpl) GetTableMetadata(ctx context.Context, projectID string, datasetID string, tableID string) (*bigquery.TableMetadata, error) {
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	return client.Dataset(datasetID).Table(tableID).Metadata(ctx)
+}
+
+func Load(project_id string, load_datasetid string, load_tableid string, schema_path string) {
+	ctx := context.Background()
+	projectID := project_id
 	datasetID := load_datasetid
 	tableID := load_tableid
+	schemaPath := schema_path
 
-	// テーブルのメタデータを取得します
-	meta, err := client.Dataset(datasetID).Table(tableID).Metadata(ctx)
+	fieldTypeMap := NewFieldTypeMap()
+	bqClient := NewBigQueryClientImpl()
+	useCase := NewSchemaUseCase(bqClient, fieldTypeMap)
+
+	err := useCase.GenerateAndSaveSchema(ctx, projectID, datasetID, tableID, schemaPath)
 	if err != nil {
-		log.Fatalf("Failed to get table metadata: %v", err)
+		log.Fatal(err)
 	}
 
-	// スキーマをJSON形式に変換します
-	schemaJSON, err := json.MarshalIndent(meta.Schema, "", "  ")
-	if err != nil {
-		log.Fatalf("Failed to marshal schema to JSON: %v", err)
-	}
-
-	// JSONファイルにスキーマを保存します
-	err = ioutil.WriteFile(schema_path, schemaJSON, 0644)
-	if err != nil {
-		log.Fatalf("Failed to write schema to file: %v", err)
-	}
-
-	fmt.Println("Schema saved to schema.json")
+	fmt.Println("Fields saved to schema.json")
 }
